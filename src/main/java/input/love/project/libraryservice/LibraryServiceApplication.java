e6564293d1ae8a5762c66b55b5011dd77fba2b65
Андрей Самардак <andrew-mac@MacBook-Air-Andrej.local>
2024-03-01 16:49:53 +0300

    init project

---
+++ src/main/java/input/love/project/libraryservice/LibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package input.love.project.libraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/input/love/project/libraryservice/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package input.love.project.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
